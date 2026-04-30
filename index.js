2ca8db528e8a2e1e25d8e2bf7f05ad460254076f
Hukmyster <huklmyster@gmail.com>
2026-05-01 00:34:06 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,12 @@
-const { Client, GatewayIntentBits, Collection } = require("discord.js");
+const { Client, GatewayIntentBits } = require("discord.js");
 require("dotenv").config();
 
 const client = new Client({
     intents: [GatewayIntentBits.Guilds]
 });
 
-client.commands = new Collection();
-
 client.once("ready", () => {
     console.log(`Logged in as ${client.user.tag}`);
-});
-
-client.on("interactionCreate", async (interaction) => {
-    if (!interaction.isChatInputCommand()) return;
-
-    const command = client.commands.get(interaction.commandName);
-    if (!command) return;
-
-    try {
-        await command.execute(interaction);
-    } catch (err) {
-        console.error(err);
-        await interaction.reply({
-            content: "Error executing command.",
-            ephemeral: true
-        });
-    }
 });
 
 client.login(process.env.DISCORD_TOKEN);
