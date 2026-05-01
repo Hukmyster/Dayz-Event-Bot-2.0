69f5f44a3c1e81d8bd59c8b87e725180247c8500
Hukmyster <huklmyster@gmail.com>
2026-05-01 20:24:33 +1200

    Update xml.js

--- modules/xml.js
+++ modules/xml.js
@@ -1,65 +1,48 @@
 const fs = require("fs");
 
-function ensure() {
-    if (!fs.existsSync("./custom")) {
-        fs.mkdirSync("./custom", { recursive: true });
-    }
-}
-
-async function buildXML(db) {
-
-    ensure();
+function buildXML(db) {
 
     const orders = db.getOrders();
 
-    let events = [];
-    let spawns = [];
+    let events = "";
+    let spawns = "";
 
     for (const o of orders) {
 
-        if (o.status !== "queued") continue;
+        const id = `ShopEvent_${Date.now()}_${Math.floor(Math.random() * 999)}`;
 
-        const qty = o.quantity || 1;
-        const name = `ShopEvent_${Date.now()}_${Math.floor(Math.random() * 9999)}`;
+        events += `
+<event name="${id}">
+<nominal>1</nominal>
+<min>1</min>
+<max>1</max>
+<lifetime>3000</lifetime>
+<restock>3888000</restock>
+<saferadius>0</saferadius>
+<distanceradius>0</distanceradius>
+<cleanupradius>0</cleanupradius>
+<flags deletable="0" init_random="0" remove_damaged="1"/>
+<position>fixed</position>
+<limit>child</limit>
+<active>1</active>
+<children>
+<child lootmax="0" lootmin="0" max="${o.quantity}" min="${o.quantity}" type="${o.type}"/>
+</children>
+</event>`;
 
-        events.push(`
-<event name="${name}">
-    <nominal>1</nominal>
-    <min>1</min>
-    <max>1</max>
-    <lifetime>3000</lifetime>
-    <restock>3888000</restock>
-    <saferadius>0</saferadius>
-    <distanceradius>0</distanceradius>
-    <cleanupradius>0</cleanupradius>
-    <flags deletable="0" init_random="0" remove_damaged="1"/>
-    <position>fixed</position>
-    <limit>child</limit>
-    <active>1</active>
-    <children>
-        <child max="${qty}" min="${qty}" type="${o.type}"/>
-    </children>
-</event>`);
-
-        spawns.push(`
-<event name="${name}">
-    <pos x="${o.x}" z="${o.z}" a="0"/>
-</event>`);
-
-        o.status = "built";
+        spawns += `
+<event name="${id}">
+<pos x="${o.x}" z="${o.z}" a="0" />
+</event>`;
     }
 
     fs.writeFileSync("./custom/shopevents.xml",
-`<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<events>${events.join("\n")}</events>`);
+        `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n<events>${events}\n</events>`
+    );
 
     fs.writeFileSync("./custom/cfgeventspawns.xml",
-`<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<eventposdef>${spawns.join("\n")}</eventposdef>`);
-
-    db.saveOrders(orders);
-
-    console.log("[XML] Built");
+        `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n<eventposdef>${spawns}\n</eventposdef>`
+    );
 }
 
 module.exports = { buildXML };
