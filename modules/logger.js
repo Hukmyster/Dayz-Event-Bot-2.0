1b78db9d522372603585da090c946faef131ef02
Hukmyster <huklmyster@gmail.com>
2026-05-01 22:47:44 +1200

    Create logger.js

---
+++ modules/logger.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+
+const LOG_FILE = path.join(__dirname, "../logs/debug.log");
+
+function ensureLogFile() {
+  const dir = path.dirname(LOG_FILE);
+  if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
+}
+
+function writeLog(type, data) {
+  ensureLogFile();
+
+  const time = new Date().toISOString();
+  const line = `[${time}] [${type}] ${typeof data === "string" ? data : JSON.stringify(data, null, 2)}\n`;
+
+  fs.appendFileSync(LOG_FILE, line);
+  console.log(`[${type}]`, data);
+}
+
+module.exports = {
+  log: (msg) => writeLog("INFO", msg),
+  warn: (msg) => writeLog("WARN", msg),
+  error: (msg, err) => writeLog("ERROR", { msg, err: err?.stack || err }),
+  interaction: (data) => writeLog("INTERACTION", data),
+  shop: (data) => writeLog("SHOP", data),
+  xml: (data) => writeLog("XML", data)
+};
