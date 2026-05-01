f4ca0cf915605b14a9a21a0bd7eb289879325850
Hukmyster <huklmyster@gmail.com>
2026-05-01 16:39:50 +1200

    Update commands.js

--- modules/commands.js
+++ modules/commands.js
@@ -1,1 +1,27 @@
+const { SlashCommandBuilder } = require("discord.js");
 
+module.exports = [
+
+    new SlashCommandBuilder().setName("shop").setDescription("View shop"),
+
+    new SlashCommandBuilder().setName("additem").setDescription("Add item"),
+
+    new SlashCommandBuilder()
+        .setName("buy")
+        .setDescription("Buy item")
+        .addStringOption(o =>
+            o.setName("item").setDescription("Item").setAutocomplete(true).setRequired(true)
+        )
+        .addIntegerOption(o =>
+            o.setName("x").setDescription("X coord").setRequired(true)
+        )
+        .addIntegerOption(o =>
+            o.setName("z").setDescription("Z coord").setRequired(true)
+        ),
+
+    new SlashCommandBuilder().setName("orders").setDescription("View orders"),
+    new SlashCommandBuilder().setName("queue").setDescription("Queue orders"),
+    new SlashCommandBuilder().setName("build").setDescription("Build XML"),
+    new SlashCommandBuilder().setName("cycle").setDescription("Complete orders"),
+    new SlashCommandBuilder().setName("viewxml").setDescription("View XML")
+];
