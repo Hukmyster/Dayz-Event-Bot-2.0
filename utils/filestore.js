ba63a321e047fe3077b6cf7d9046e9a5a1ac65c2
Hukmyster <huklmyster@gmail.com>
2026-05-02 00:13:25 +1200

    Update filestore.js

--- utils/filestore.js
+++ utils/filestore.js
@@ -1,1 +1,27 @@
+const fs = require("fs");
+const path = require("path");
 
+function ensure(file) {
+  const dir = path.dirname(file);
+  if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
+  if (!fs.existsSync(file)) fs.writeFileSync(file, JSON.stringify([]));
+}
+
+function read(file) {
+  ensure(file);
+  try {
+    return JSON.parse(fs.readFileSync(file, "utf-8"));
+  } catch {
+    return [];
+  }
+}
+
+function write(file, data) {
+  ensure(file);
+  fs.writeFileSync(file, JSON.stringify(data, null, 2));
+}
+
+module.exports = {
+  read,
+  write
+};
