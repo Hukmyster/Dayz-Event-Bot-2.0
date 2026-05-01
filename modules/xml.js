81fe809382a431512ae7516bde4be4dac762f3a9
Hukmyster <huklmyster@gmail.com>
2026-05-01 19:43:20 +1200

    Update xml.js

--- modules/xml.js
+++ modules/xml.js
@@ -1,11 +1,25 @@
 const fs = require("fs");
+const path = require("path");
 const db = require("../services/db");
+
+// ALWAYS ensure folder exists BEFORE ANY WRITE
+function ensureCustomFolder() {
+    const dir = path.join(__dirname, "../custom");
+
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
+    }
+
+    return dir;
+}
 
 function makeEventName() {
     return `ShopEvent_${Date.now()}_${Math.floor(Math.random() * 1000)}`;
 }
 
 async function buildXML() {
+
+    ensureCustomFolder();
 
     const orders = db.getOrders();
 
@@ -17,38 +31,32 @@ async function buildXML() {
         if (o.status !== "queued") continue;
 
         const eventName = makeEventName();
-
-        const lifetime = 3000;
-        const restock = 3888000;
-
         const qty = o.quantity || 1;
 
-        // ---------------- EVENT XML ----------------
         events.push(`
-    <event name="${eventName}">
-        <nominal>1</nominal>
-        <min>1</min>
-        <max>1</max>
-        <lifetime>${lifetime}</lifetime>
-        <restock>${restock}</restock>
-        <saferadius>0</saferadius>
-        <distanceradius>0</distanceradius>
-        <cleanupradius>0</cleanupradius>
-        <flags deletable="0" init_random="0" remove_damaged="1"/>
-        <position>fixed</position>
-        <limit>child</limit>
-        <active>1</active>
-        <children>
-            <child lootmax="0" lootmin="0" max="${qty}" min="${qty}" type="${o.itemType}"/>
-        </children>
-    </event>
+<event name="${eventName}">
+    <nominal>1</nominal>
+    <min>1</min>
+    <max>1</max>
+    <lifetime>3000</lifetime>
+    <restock>3888000</restock>
+    <saferadius>0</saferadius>
+    <distanceradius>0</distanceradius>
+    <cleanupradius>0</cleanupradius>
+    <flags deletable="0" init_random="0" remove_damaged="1"/>
+    <position>fixed</position>
+    <limit>child</limit>
+    <active>1</active>
+    <children>
+        <child lootmax="0" lootmin="0" max="${qty}" min="${qty}" type="${o.itemType}"/>
+    </children>
+</event>
         `.trim());
 
-        // ---------------- SPAWN XML ----------------
         spawns.push(`
-    <event name="${eventName}">
-        <pos x="${o.x}" z="${o.z}" a="0" />
-    </event>
+<event name="${eventName}">
+    <pos x="${o.x}" z="${o.z}" a="0" />
+</event>
         `.trim());
 
         o.status = "built";
@@ -66,12 +74,14 @@ ${events.join("\n")}
 ${spawns.join("\n")}
 </eventposdef>`;
 
-    fs.writeFileSync("./custom/shopevents.xml", eventXML);
-    fs.writeFileSync("./custom/cfgeventspawns.xml", spawnXML);
+    const dir = ensureCustomFolder();
+
+    fs.writeFileSync(path.join(dir, "shopevents.xml"), eventXML);
+    fs.writeFileSync(path.join(dir, "cfgeventspawns.xml"), spawnXML);
 
     db.saveOrders(orders);
 
-    console.log("[XML] Built clean structured XML");
+    console.log("[XML] Build successful");
 }
 
 module.exports = { buildXML };
