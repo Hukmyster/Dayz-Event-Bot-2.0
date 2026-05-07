b67ef6662ac3fe593ad33480d8cbf6ecaa8381f4
Hukmyster <huklmyster@gmail.com>
2026-05-07 18:06:19 +1200

    Update indexcommands.js

--- indexcommands.js
+++ indexcommands.js
@@ -1,1 +1,18 @@
+const { handleAutocomplete, handleButton, handleCommand } = require("./indexcommandscore");
 
+async function handleInteraction(interaction) {
+  if (interaction.isAutocomplete()) {
+    return handleAutocomplete(interaction);
+  }
+
+  if (interaction.isButton()) {
+    const handled = await handleButton(interaction);
+    if (handled !== false) return handled;
+  }
+
+  if (!interaction.isChatInputCommand()) return;
+
+  return handleCommand(interaction);
+}
+
+module.exports = { handleInteraction };
