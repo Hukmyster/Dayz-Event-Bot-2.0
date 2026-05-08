c4975ce462dfb3fad0b5f2ed91a42d5ada7163ca
Hukmyster <huklmyster@gmail.com>
2026-05-09 01:20:39 +1200

    Update radarview.js

--- commands/radar/radarview.js
+++ commands/radar/radarview.js
@@ -1,5 +1,46 @@
+const fs = require("fs");
+const path = require("path");
+
+const radarDir = "/dayzps_missions/dayzOffline.chernarusplus/custom/server/radars";
+
 const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder, MessageFlags } = require("discord.js");
-const { listRadars } = require("../../modules/playerradars");
+
+function replyEphemeral(interaction, content) {
+  return interaction.reply({ content, flags: MessageFlags.Ephemeral });
+}
+
+async function listRadars() {
+  try {
+    const dirExistsStat = fs.statSync(radarDir);
+    if (!dirExistsStat.isDirectory()) {
+      return [];
+    }
+  } catch (errDir) {
+    return [];
+  }
+
+  const files = await fs.promises.readdir(radarDir);
+
+  const radars = [];
+
+  for (const file of files) {
+    if (!file.endsWith(".json")) continue;
+
+    const filePath = path.join(radarDir, file);
+    try {
+      const text = await fs.promises.readFile(filePath, "utf8");
+      const data = JSON.parse(text);
+
+      if (data.name && data.x != null && data.z != null && data.radius) {
+        radars.push(data);
+      }
+    } catch (err) {
+      console.error("Failed to read radar file:", filePath, err);
+    }
+  }
+
+  return radars;
+}
 
 module.exports = {
   data: new SlashCommandBuilder()
