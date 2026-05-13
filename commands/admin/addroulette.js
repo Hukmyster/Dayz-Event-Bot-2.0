7224e68ef0ba8c0a09950affbba7c848a7b16d5c
Hukmyster <huklmyster@gmail.com>
2026-05-13 22:39:15 +1200

    Update addroulette.js

--- commands/admin/addroulette.js
+++ commands/admin/addroulette.js
@@ -1,5 +1,4 @@
 const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require("discord.js");
-const casino = require("../../modules/casino/casino");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -10,12 +9,30 @@ module.exports = {
   async execute(interaction) {
     await interaction.deferReply({ ephemeral: true });
 
-    const embed = casino.roulette?.createLauncherEmbed
-      ? casino.roulette.createLauncherEmbed()
-      : new EmbedBuilder()
-          .setTitle("🎲 Roulette")
-          .setDescription("Click **Play** to start your private roulette session.")
-          .setColor(0x000000);
+    const embed = new EmbedBuilder()
+      .setTitle("🎲 Roulette")
+      .setDescription(
+        "**How to play**\n" +
+        "1. Click the `Play` button.\n" +
+        "2. Open your private session.\n" +
+        "3. Set bet amount, choose bet type, then spin.\n\n" +
+        "**Payouts**\n" +
+        "🟢 Green (0): 10x your bet\n" +
+        "🔴 Red (1-18): 2x your bet\n" +
+        "⚫ Black (19-36): 2x your bet\n\n" +
+        "**Session**\n" +
+        "• Each player gets their own private session.\n" +
+        "• You can spin again and change your bet anytime before timeout.\n" +
+        "• Session expires after inactivity.\n\n" +
+        "Maximum bet: 100,000.\n" +
+        "You must have enough in your wallet for your bet."
+      )
+      .setColor(0x000000)
+      .addFields(
+        { name: "Bet", value: "0", inline: true },
+        { name: "Number", value: "–", inline: true },
+        { name: "Wallet", value: "0", inline: true }
+      );
 
     const playButton = new ButtonBuilder()
       .setCustomId("casino:roulette:play")
